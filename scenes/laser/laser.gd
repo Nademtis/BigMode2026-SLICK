e3759281c677f95d721e8f5cdb20ab76c27999a7
Nademtis <mathias.99.madsen@gmail.com>
2026-02-06 22:15:21 +0100

    more lasssserrrr

--- scenes/laser/laser.gd
+++ scenes/laser/laser.gd
@@ -7,14 +7,38 @@ enum Direction {
 @onready var collision_polygon_2d: CollisionPolygon2D = $Area2D/CollisionPolygon2D
 @export var direction: Direction = Direction.RIGHT_LEFT
 
+@onready var shadow_line_2d: Line2D = $shadowLine2d
 
 func _ready() -> void:
 	set_coll()
-
-
+	set_shadow()
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("laser found player")
+
+func set_shadow() -> void:
+	if direction == Direction.RIGHT_LEFT:
+		shadow_line_2d.points = self.points
+		shadow_line_2d.position = shadow_line_2d.position + Vector2(0, 5)
+		#var new_polygon : PackedVector2Array
+		#var point_1 : Vector2
+		#var point_2 : Vector2
+		#var point_3 : Vector2
+		#var point_4 : Vector2
+#
+		#point_1 = Vector2(points[0].x, points[0].y - 1)
+		#point_2 = Vector2(points[0].x, points[0].y + 1)
+		#
+		#point_3 = Vector2(points[1].x, points[1].y - 1)
+		#point_4 = Vector2(points[1].x, points[1].y + 1)
+#
+		#new_polygon.append(point_1)
+		#new_polygon.append(point_2)
+		#new_polygon.append(point_3)
+		#new_polygon.append(point_4)
+		#
+		#shadow_line_2d.points = new_polygon
 
 func set_coll() -> void:
 	if points.is_empty():
@@ -30,7 +54,7 @@ func set_coll() -> void:
 		point_1 = Vector2(points[0].x - 1, points[0].y)
 		point_2 = Vector2(points[0].x + 1, points[0].y)
 		
-		point_3 = Vector2(points[1].x + 1, points[1].y)
+		point_3 = Vector2(points[1].x - 1, points[1].y)
 		point_4 = Vector2(points[1].x + 1, points[1].y)
 		
 		new_coll_polygon.append(point_1)
@@ -43,7 +67,7 @@ func set_coll() -> void:
 		point_1 = Vector2(points[0].x, points[0].y - 1)
 		point_2 = Vector2(points[0].x, points[0].y + 1)
 		
-		point_3 = Vector2(points[1].x, points[1].y + 1)
+		point_3 = Vector2(points[1].x, points[1].y - 1)
 		point_4 = Vector2(points[1].x, points[1].y + 1)
 		
 		new_coll_polygon.append(point_1)
