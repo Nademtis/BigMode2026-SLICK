d6386a019fa9156d56a547da39af33514c5e8f6d
Nademtis <mathias.99.madsen@gmail.com>
2026-02-06 21:08:42 +0100

    camera somewhat done for now

--- scenes/camera/s_camera.gd
+++ scenes/camera/s_camera.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 #Camera
 
@@ -6,14 +7,62 @@ extends Node2D
 @onready var polygon_2d: Polygon2D = $rotater/Area2D/Polygon2D
 @onready var collision_polygon_2d: CollisionPolygon2D = $rotater/Area2D/CollisionPolygon2D
 
+@export var marker_list : Array[Marker2D]
+@export var duration_list: Array[float] = []
+#@export var idle_time : float = 5
+@export var rotation_speed: float = 5.0 # higher = faster
+
+@onready var timer: Timer = $Timer
+
+var target_rotation: float = 0.0
+var current_marker_index : int = 0
 
 func _ready() -> void:
 	var polygon : PackedVector2Array = collision_polygon_2d.polygon
 	polygon_2d.polygon = polygon
 	
+	#error handling
+	if marker_list.is_empty() or duration_list.is_empty():
+		push_error("markerlist or duration is empty on a camera")
 	
+	if marker_list.size() == 1:
+		look_at_marker(marker_list[0], 0)
+	else:
+		start_look_sequence()
 
+
+func _process(delta: float) -> void:
+	rotater.rotation = lerp_angle(rotater.rotation, target_rotation, delta * rotation_speed)
+
+
+func look_at_marker(marker : Marker2D, duration : float) -> void:
+	var dir: Vector2 = (marker.global_position - global_position).normalized()
+	target_rotation = dir.angle() - PI / 2
+	
+	if duration > 0:
+		timer.wait_time = duration
+		timer.start()
+	else:
+		timer.stop()
+
+func start_look_sequence() -> void:
+	if marker_list.is_empty():
+		return
+	
+	current_marker_index = 0
+	look_at_marker(marker_list[current_marker_index], duration_list[current_marker_index])
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("camera found player")
+
+
+func _on_timer_timeout() -> void:
+	current_marker_index += 1
+
+	if current_marker_index >= marker_list.size():
+		current_marker_index = 0
+
+	var next_marker: Marker2D = marker_list[current_marker_index]
+	var next_duration: float = duration_list[current_marker_index]
+	look_at_marker(next_marker, next_duration)
